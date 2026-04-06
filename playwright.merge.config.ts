7127909555cd441f741b395cebe63f286ae8f2c6
NandiniMonpara <nandinimonpara1@gmail.com>
2026-04-06 14:54:18 +0530

    fix:added json reporting

---
+++ playwright.merge.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  reporter: [
+    [
+      'html',
+      {
+        outputFolder: 'playwright-report',
+        open: 'never',
+      },
+    ],
+    ['json', { outputFile: 'playwright-report/report.json' }],
+  ],
+});
